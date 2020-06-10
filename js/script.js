1c45e4912c897995a70d2967395c5016ac17d714
Aldo Cahuana <aldooow@icloud.com>
2020-06-10 18:18:27 +0200

    Work in progress!!!!!

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(
+
   function(){
+    $('.with-dropdown').mouseenter(
+      function(){
+        $(this).children('.dropdown').toggleClass('open');
+      }
+    )
+
     $('.with-dropdown').click(
       function(){
         $(this).children('.dropdown').toggleClass('open');
       }
-
     )
   }
+
+
 );
