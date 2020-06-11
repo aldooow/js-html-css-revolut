b5b96e4814c99b3a5f5983936690a9f03ceb5033
Aldo Cahuana <aldooow@icloud.com>
2020-06-11 11:35:47 +0200

    Work in progress!!!!

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 $(document).ready(
 
-  function(){
-    $('.with-dropdown').mouseenter(
-      function(){
-        $(this).children('.dropdown').toggleClass('open');
-        $('.dropdown').removeClass('open')
-      }
-    )
 
+  function(){
+    // Click nel "document" per nascondere le opzioni.
+    $(document).click(
+      function(){
+        $('.dropdown').removeClass('open');
+      }
+    );
+
+    // Click su .... , per aprire le opzioni
     $('.with-dropdown').click(
       function(){
+        // ".siblings" --> prende i fratelli di un elemento.
+        $('.dropdown.open').siblings().removeClass('open');
         $(this).children('.dropdown').toggleClass('open');
       }
-    )
-  }
+    );
 
+    // Mouse Enter
+    $('.with-dropdown').mouseenter(
+      function(){
+        $('.dropdown.open').removeClass('open');
+        $(this).children('.dropdown').toggleClass('open');
+
+      }
+    );
+
+    // Mouse Leave... (X) In questo caso NO!!!
+
+  }
 
 );
